72e9732f15c9bbf96592b38a73783291a9aa288e
Chris Chavez <127803129+cachav19@users.noreply.github.com>
2023-05-25 01:51:23 -0500

    hotfix#4

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -265,6 +265,9 @@ class WebServer {
             }
 
           } catch (Exception ex) {
+            builder.append("HTTP/1.1 400 Bad Request\n");
+            builder.append("Content-Type: text/html; charset=utf-8\n");
+            builder.append("\n");
             builder.append("Something went wrong.");
           }
 
